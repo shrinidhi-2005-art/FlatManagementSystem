94e9107abf6f150a5972b6f226be344b51186a0b
shrinidhi-2005-art <write2shrinidhi@gmail.com>
2025-01-29 20:42:20 +0530

    Update dsa.c

--- dsa.c
+++ dsa.c
@@ -5,7 +5,7 @@
 #include "flats.h"
 
 const char dataset[50] = "data.csv";
-int current = 0; 
+int current= 0; 
 flat residence[size];
 
 int main(){
@@ -13,7 +13,7 @@ int main(){
     read_file(); //reads the file data.csv and saves all the info to the RAM (residence array)
     menufn(); //calls the main menu funtion
 
-    return 0;
+    return 0;;
 }
 
 //ver 2.1.0 stable
